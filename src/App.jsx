7de4f279fe5905bae00df39b29e6fb9bf9aef003
Beni Vucsics <vucben99@gmail.com>
2022-11-29 15:23:58 +0100

    megakadás

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+  const [imgList, setImgList] = useState({})
+
+  const loadData = async () => {
+    const res = await fetch("https://api.harvardartmuseums.org/object?size=10&page=1&apikey=68e5277d-7e38-47b2-9cc3-093891d90747")
+    const resJson = await res.json()
+    setImgList(resJson)
+    console.log(imgList)
+  }
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
   return (
     <div className="App">
-      
+      Hello world
     </div>
   )
 }
