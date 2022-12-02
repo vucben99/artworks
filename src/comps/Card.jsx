315948cd67746a2c1b4c4c78f3251157195bf6cb
kallosbela <kallosbela@gmail.com>
2022-12-02 18:00:06 +0100

    nincs sarki kisbolt

--- src/comps/Card.jsx
+++ src/comps/Card.jsx
@@ -1,27 +1,57 @@
-import { useState, useEffect } from 'react'
-import './Card.css'
-import { AiOutlineHeart, AiFillHeart } from "react-icons/ai"
-import { AiOutlineDownload } from 'react-icons/ai';
+import { useState, useEffect } from "react";
+import "./Card.css";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { AiOutlineDownload } from "react-icons/ai";
+import { saveAs } from "file-saver";
 
 function Card({ imgObj }) {
+  const [isFav, setIsFav] = useState(false);
 
-    const [isFav, setIsFav] = useState(false)
+  // const downloadImg = (url) => {
+  //   saveAs(url, "valami.jpg");
+  // };
 
-    return (
-        <li>
-            <img src={imgObj.url.replace("original","web-large")} alt={imgObj.title} />
-            <div className='card-text'>
-                <h2>
-                    {imgObj.title}
-                    {!isFav ? <AiOutlineHeart className="heart-icon" onClick={() => setIsFav(!isFav)} /> : <AiFillHeart className="heart-icon" fill="#ff002f" onClick={() => setIsFav(!isFav)} />}
-                </h2>
-                <p>Artist: {imgObj.artist}</p>
-                <p>Date: {imgObj.date}</p>
-                <p>More info <a href={imgObj.description}>here</a></p>
-                <p><a href={imgObj.url} download={"./"+imgObj.id+"jpg"} target="_blank"><AiOutlineDownload/></a></p>
-            </div>
-        </li>
-    )
+  return (
+    <li>
+      <img
+        src={imgObj.url.replace("original", "web-large")}
+        alt={imgObj.title}
+        // onClick={downloadImg(imgObj.url)}
+      />
+      <div className="card-text">
+        <h2>
+          {imgObj.title}
+          {!isFav ? (
+            <AiOutlineHeart
+              className="heart-icon"
+              onClick={() => setIsFav(!isFav)}
+            />
+          ) : (
+            <AiFillHeart
+              className="heart-icon"
+              fill="#ff002f"
+              onClick={() => setIsFav(!isFav)}
+            />
+          )}
+        </h2>
+        <p>Artist: {imgObj.artist}</p>
+        <p>Date: {imgObj.date}</p>
+        <p>
+          More info <a href={imgObj.description}>here</a>
+        </p>
+        <p>
+          <a
+            href={imgObj.url}
+            target="_blank"
+            download={imgObj.url}
+            // onClick={downloadImg(imgObj.url)}
+          >
+            <AiOutlineDownload />
+          </a>
+        </p>
+      </div>
+    </li>
+  );
 }
 
-export default Card
+export default Card;
